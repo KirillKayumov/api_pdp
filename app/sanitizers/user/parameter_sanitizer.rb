78de59c5445825f45f5458fe24b792c861e89ce5
Kirill Kayumov <kirill.kayumov@flatstack.com>
2016-09-14 16:34:15 +0300

    Add ability to update profile

--- app/sanitizers/user/parameter_sanitizer.rb
+++ app/sanitizers/user/parameter_sanitizer.rb
@@ -1,17 +1,22 @@
 class User
   class ParameterSanitizer < Devise::ParameterSanitizer
     USER_PARAMS = %i(
+      first_name
+      last_name
+      bio
       email
       password
       password_confirmation
     ).freeze
+
+    ACCOUNT_UPDATE_PARAMS = (USER_PARAMS + %i(current_password)).freeze
 
     def sign_up
       default_params.permit(USER_PARAMS)
     end
 
     def account_update
-      default_params.permit(USER_PARAMS)
+      default_params.permit(ACCOUNT_UPDATE_PARAMS)
     end
   end
 end
